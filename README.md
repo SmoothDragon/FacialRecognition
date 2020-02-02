da8e264c5f0625e2d933a045edfe0bf789fa8756
Tom Draper <tgdrape@gmail.com>
2020-02-01 16:34:19 -0800

    Let's get this party started.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # FacialRecognition
 Use OpenVINO with an Intel Neural Compute stick to do real time facial recognition.
+
+## OpenVINO needs to be in current path for doctor
+source /opt/intel/openvino/bin/setupvars.sh
---
+++ install.sh
@@ -0,0 +1,17 @@
+
+wget --no-check-certificate https://download.01.org/opencv/2019/open_model_zoo/R1/models_bin/face-detection-adas-0001/FP16/face-detection-adas-0001.bin
+wget --no-check-certificate https://download.01.org/opencv/2019/open_model_zoo/R1/models_bin/face-detection-adas-0001/FP16/face-detection-adas-0001.xml
+
+wget --no-check-certificate https://download.01.org/opencv/2019/open_model_zoo/R1/models_bin/face-detection-retail-0004/FP16/face-detection-retail-0004.bin
+wget --no-check-certificate https://download.01.org/opencv/2019/open_model_zoo/R1/models_bin/face-detection-retail-0004/FP16/face-detection-retail-0004.xml
+
+wget --no-check-certificate https://download.01.org/opencv/2019/open_model_zoo/R1/models_bin/landmarks-regression-retail-0009/FP16/landmarks-regression-retail-0009.bin
+wget --no-check-certificate https://download.01.org/opencv/2019/open_model_zoo/R1/models_bin/landmarks-regression-retail-0009/FP16/landmarks-regression-retail-0009.xml
+
+wget --no-check-certificate https://download.01.org/opencv/2019/open_model_zoo/R1/models_bin/face-reidentification-retail-0095/FP16/face-reidentification-retail-0095.bin
+wget --no-check-certificate https://download.01.org/opencv/2019/open_model_zoo/R1/models_bin/face-reidentification-retail-0095/FP16/face-reidentification-retail-0095.xml
+
+# https://download.01.org/opencv/2019/open_model_zoo/R1/models_bin/face-reidentification-retail-0095/FP16/face-reidentification-retail-0095.bin
+
+# Demo repository
+# git clone https://github.com/opencv/open_model_zoo.git
