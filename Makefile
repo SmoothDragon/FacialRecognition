06a33ae2e9788ba6b57989b649adebfbf6b710d6
Thomas Draper <tom@betterservers.com>
2020-02-22 13:19:20 -0800

    First working demo

---
+++ Makefile
@@ -0,0 +1,3 @@
+install:
+	ln -fs /opt/intel/openvino/deployment_tools/open_model_zoo/demos/python_demos/face_recognition_demo/ .
+	pip install -r requirements.txt
---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+BASE_DIR=($PWD)
+echo $BASE_DIR
+
+cd face_recognition_demo
+python ./face_recognition_demo.py \
+  -m_fd $BASE_DIR/models/face-detection-retail-0004.xml \
+  -m_lm $BASE_DIR/models/landmarks-regression-retail-0009.xml \
+  -m_reid $BASE_DIR/models/face-reidentification-retail-0095.xml \
+  -l /opt/intel/openvino/deployment_tools/inference_engine/lib/intel64/libcpu_extension_sse4.so \
+  --verbose \
+  -fg $BASE_DIR/face_gallery
+
