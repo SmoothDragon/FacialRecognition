23462881113f259f4c76b605313a217dd00d3d09
Thomas Draper <tom@betterservers.com>
2020-02-22 12:21:40 -0800

    Update installation framework.

--- get_models.sh
+++ get_models.sh
@@ -1,4 +1,5 @@
 
+cd models
 wget --no-check-certificate https://download.01.org/opencv/2019/open_model_zoo/R1/models_bin/face-detection-adas-0001/FP16/face-detection-adas-0001.bin
 wget --no-check-certificate https://download.01.org/opencv/2019/open_model_zoo/R1/models_bin/face-detection-adas-0001/FP16/face-detection-adas-0001.xml
 
